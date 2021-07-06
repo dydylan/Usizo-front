883b431aed0002fe4ab75ab18099c15d2878c74d
DSARRAZY <d.sarrazy@gmail.com>
2021-07-06 15:35:15 +0200

    contrainte formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './composants/login/login.component';
 import { SignUpComponent } from './composants/sign-up/sign-up.component';
 import { FooterComponent } from './composants/footer/footer.component';
 import { HeaderComponent } from './composants/header/header.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { HeaderComponent } from './composants/header/header.component';
     FooterComponent,
     HeaderComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
