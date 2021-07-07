61cc14355d55ac9eab1be91506f143446b1792ce
Illusion-97 <56721402+Illusion-97@users.noreply.github.com>
2021-07-07 09:25:48 +0200

    linked

--- src/app/composants/shopping-list-manager/shopping-list-manager.component.ts
+++ src/app/composants/shopping-list-manager/shopping-list-manager.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../../models/Product";
+import {ProductsService} from "../../services/products.service";
+import {ActivatedRoute} from "@angular/router";
+import {ShoppingList} from "../../models/ShoppingList";
+import {ShoppingListService} from "../../services/shopping-list.service";
+import {listLazyRoutes} from "@angular/compiler/src/aot/lazy_routes";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Need} from "../../models/Need";
+import {NeedService} from "../../services/need.service";
 
 @Component({
   selector: 'app-shopping-list-manager',
@@ -7,11 +16,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListManagerComponent implements OnInit {
 
-  constructor() { }
+  private _products:Product[] = []
+  private _shoppingList:ShoppingList = new ShoppingList(NaN,"",[],[])
+  private _formProd:FormGroup;
+  private _qte:FormControl
 
-  ngOnInit(): void {
+  constructor(private needService:NeedService, private productService:ProductsService, private shoppingListService:ShoppingListService, private activeRoute:ActivatedRoute,private formBuilder:FormBuilder) {
+    this._qte=new FormControl('',[Validators.required])
+    this._formProd=this.formBuilder.group({
+      qte:this._qte
+    })
   }
 
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe(params =>{
+      this.shoppingListService.get(params["id"]).subscribe(list => {
+        this._shoppingList = list;
+        console.log(this._shoppingList.needList)
+      })
+    })
+    this.productService.getAll().subscribe(prods => {
+      this._products = prods
+    })
+  }
+
+  get products(): Product[] {
+    return this._products;
+  }
+
+  set products(value: Product[]) {
+    this._products = value;
+  }
+
+  get shoppingList(): ShoppingList {
+    return this._shoppingList;
+  }
+
+  set shoppingList(value: ShoppingList) {
+    this._shoppingList = value;
+  }
+
+  get formProd(): FormGroup {
+    return this._formProd;
+  }
+
+  set formProd(value: FormGroup) {
+    this._formProd = value;
+  }
+
+  get qte(): FormControl {
+    return this._qte;
+  }
+
+  set qte(value: FormControl) {
+    this._qte = value;
+  }
+
+  addProd(id:number){
+    this.productService.get(id.toString()).subscribe(prod =>
+    {
+      this.needService.create(this._shoppingList.id,id,this._formProd.get('qte')?.value).subscribe(need =>
+      {
+
+      })
+
+    })
+  }
 }
 
 
