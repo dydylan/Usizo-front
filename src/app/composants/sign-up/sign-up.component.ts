176f375933c04695d3560233a2c969a1cb26e6ab
DSARRAZY <d.sarrazy@gmail.com>
2021-07-09 12:22:44 +0200

    signin et signup

--- src/app/composants/sign-up/sign-up.component.ts
+++ src/app/composants/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 import { MustMatch } from '../../_helpers/must-match.validator';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -29,14 +30,11 @@ export class SignUpComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
-
     // stop here if form is invalid
     if (this.registerForm.invalid) {
       return;
     }
 
-    // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
   }
 
   onReset() {
@@ -45,3 +43,35 @@ export class SignUpComponent implements OnInit {
   }
 
 }
+
+//TUTO
+// export class RegisterComponent implements OnInit {
+//   form: any = {
+//     username: null,
+//     email: null,
+//     password: null
+//   };
+//   isSuccessful = false;
+//   isSignUpFailed = false;
+//   errorMessage = '';
+//
+//   constructor(private authService: AuthService) { }
+//
+//   ngOnInit(): void {
+//   }
+//
+//   onSubmit(): void {
+//     const { username, email, password } = this.form;
+//
+//     this.authService.register(username, email, password).subscribe(
+//       data => {
+//         console.log(data);
+//         this.isSuccessful = true;
+//         this.isSignUpFailed = false;
+//       },
+//       err => {
+//         this.errorMessage = err.error.message;
+//         this.isSignUpFailed = true;
+//       }
+//     );
+//   }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../models/User";
 
+const API_URL = 'http://localhost:8080/api/test/';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,5 +14,21 @@ export class UserService {
 
   get(id:String): Observable<User> {
     return this.http.get<User>("http://localhost:8080/api/user?id="+id)
+  }
+
+  getPublicContent(): Observable<any> {
+    return this.http.get(API_URL + 'all', { responseType: 'text' });
+  }
+
+  getUserBoard(): Observable<any> {
+    return this.http.get(API_URL + 'user', { responseType: 'text' });
+  }
+
+  getModeratorBoard(): Observable<any> {
+    return this.http.get(API_URL + 'mod', { responseType: 'text' });
+  }
+
+  getAdminBoard(): Observable<any> {
+    return this.http.get(API_URL + 'admin', { responseType: 'text' });
   }
 }
