137fb5d5a922f6d22b82c8b4a352a176495bdda6
DSARRAZY <d.sarrazy@gmail.com>
2021-07-09 12:22:44 +0200

    signin et signup

--- src/app/composants/login/login.component.ts
+++ src/app/composants/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { AuthService } from '../../services/auth.service';
+import { TokenStorageService } from '../../services/token-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -19,6 +21,7 @@ export class LoginComponent implements OnInit {
       name: ['', Validators.compose([Validators.required])],
       password: ['', Validators.compose([Validators.required])],
     });
+
   }
 
   get f() { return this.connectionForm.controls; }
@@ -30,8 +33,51 @@ export class LoginComponent implements OnInit {
     if (this.connectionForm.invalid) {
       return;
     }
-
-    // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.connectionForm.value, null, 4));
   }
 }
+
+
+// TUTO A MODIFIER
+// export class LoginComponent implements OnInit {
+//   form: any = {
+//     username: null,
+//     password: null
+//   };
+//   isLoggedIn = false;
+//   isLoginFailed = false;
+//   errorMessage = '';
+//   roles: string[] = [];
+//
+//   constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+//
+//   ngOnInit(): void {
+//     if (this.tokenStorage.getToken()) {
+//       this.isLoggedIn = true;
+//       this.roles = this.tokenStorage.getUser().roles;
+//     }
+//   }
+//
+//   onSubmit(): void {
+//     const { username, password } = this.form;
+//
+//     this.authService.login(username, password).subscribe(
+//       data => {
+//         this.tokenStorage.saveToken(data.accessToken);
+//         this.tokenStorage.saveUser(data);
+//
+//         this.isLoginFailed = false;
+//         this.isLoggedIn = true;
+//         this.roles = this.tokenStorage.getUser().roles;
+//         this.reloadPage();
+//       },
+//       err => {
+//         this.errorMessage = err.error.message;
+//         this.isLoginFailed = true;
+//       }
+//     );
+//   }
+//
+//   reloadPage(): void {
+//     window.location.reload();
+//   }
+// }
