aae6c344b2392dc0dfcf16e32d9bc0fcda40aac1
DSARRAZY <d.sarrazy@gmail.com>
2021-07-15 13:24:11 +0200

    heroku

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ app.use(express.static("./dist/Usizo-front"));
 app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "dist/Usizo-front" });
 });
-app.listen(process.env.PORT || 8000);
-console.log("App is running on port " + port);
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log("App is running on port " + port);
+});
+
+//app.listen(process.env.PORT || 8080);
 //console.log(`Running on port ${process.env.PORT || 8080}`);
