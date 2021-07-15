a4f7475015a61dea4c81577e6baec58e2462e66c
DSARRAZY <d.sarrazy@gmail.com>
2021-07-15 15:00:12 +0200

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-app.use(express.static("./dist/Usizo-front"));
+app.use(express.static("./dist/usizo-angular"));
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/Usizo-front" });
+  res.sendFile("index.html", { root: "dist/usizo-angular" });
 });
-http.listen((process.env.PORT || 5000), function(){
-    console.log('listening on *:5000');
-});
+app.listen(process.env.PORT || 8080);{
+  console.log(`Running on port ${process.env.PORT || 8080}`);
+};
 
 
 //app.listen(process.env.PORT || 8080);
