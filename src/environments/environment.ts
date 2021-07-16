f16d88177e86fb6af9e6b5bd581d6b8ad93aa145
DSARRAZY <d.sarrazy@gmail.com>
2021-07-16 12:04:15 +0200

    heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false
 };
 
-export const API_URL = "http://localhost:8080/api"
+export const API_URL = "https://usizo-angular.herokuapp.com/api"
 
 /*
  * For easier debugging in development mode, you can import the following file
